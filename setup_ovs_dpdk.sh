f7b7d9c90b996e79c359b364e8a6dde2981f8dd1
huangqundl <huangqundl@gmail.com>
2016-08-02 22:35:14 +0800

    update start-vm

--- setup_ovs_dpdk.sh
+++ setup_ovs_dpdk.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-### setup hugetables
+### setup hugetables: 2048 * 2M pages
 mkdir /dev/hugepages
 mount -t hugetlbfs none /dev/hugepages
 
-#### load kernel module
+#modprobe openvswitch
+
+#### load vfio kernel module
 modprobe vfio-pci
 sudo chmod a+x /dev/vfio
 sudo chmod 0666 /dev/vfio/*
 
+#### load uio kernel module
 modprobe uio
 insmod /home/qhuang/workspace/dpdk-2.2.0/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
 insmod /home/qhuang/workspace/dpdk-2.2.0/lib/librte_vhost/eventfd_link/eventfd_link.ko
 
-/home/qhuang/workspace/dpdk-2.2.0/tools/dpdk_nic_bind.py --bind=vfio-pci eth0
-/home/qhuang/workspace/dpdk-2.2.0/tools/dpdk_nic_bind.py --status
+#### bind NIC for DPDK
+dpdk_nic_bind.py --bind=vfio-pci 00:19.0
+dpdk_nic_bind.py --status
 
 #### setup ovsdb
-rm -f /usr/local/etc/openvswitch/conf.db
+rm -f /etc/openvswitch/conf.db
 ovsdb-tool create /usr/local/etc/openvswitch/conf.db /usr/local/share/openvswitch/vswitch.ovsschema
 
 ### open ovsdb server (no SSL) 
@@ -29,11 +33,11 @@ ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
 ovs-vsctl --no-wait init
 
 ### open switched
-/usr/local/sbin/ovs-vswitchd --dpdk  -c 0x1 -n 4 --socket-mem 1024,0 -- unix:/usr/local/var/run/openvswitch/db.sock --pidfile --detach
+ovs-vswitchd --dpdk -c 0x1 -n 4 --socket-mem 1024,0 -- unix:/usr/local/var/run/openvswitch/db.sock --pidfile --log-file=/var/log/openvswitch/ovs-vswitchd.log
 #/usr/local/sbin/ovs-vswitchd --dpdk  -c 0x1 -n 4 -- unix:/usr/local/var/run/openvswitch/db.sock --pidfile --detach
 
-ovs-vsctl add-br br0 -- set bridge br0 datapath_type=netdev
-ovs-vsctl add-port br0  vhost-user1 -- set interface vhost-user1 type=dpdkvhostuser
-ovs-vsctl add-port br0  vhost-user2 -- set interface vhost-user2 type=dpdkvhostuser
+#ovs-vsctl add-br br0 -- set bridge br0 datapath_type=netdev
+#ovs-vsctl add-port br0  vhost-user1 -- set interface vhost-user1 type=dpdkvhostuser
+#ovs-vsctl add-port br0  vhost-user2 -- set interface vhost-user2 type=dpdkvhostuser
 #ovs-vsctl add-port br0  vhost-user2 -- set interface vhost-user2 type=dpdkvhostuser
 #ovs-vsctl add-port br0  dpdk0 -- set interface dpdk0 type=dpdk
