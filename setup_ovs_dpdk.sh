7d5b6a81070b335c9a9e7dd1fbb40617e0c2a5fe
huangqundl <huangqundl@gmail.com>
2016-08-02 14:18:50 +0800

    dpdk script

---
+++ setup_ovs_dpdk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+### setup hugetables
+mkdir /dev/hugepages
+mount -t hugetlbfs none /dev/hugepages
+
+#### load kernel module
+modprobe vfio-pci
+sudo chmod a+x /dev/vfio
+sudo chmod 0666 /dev/vfio/*
+
+modprobe uio
+insmod /home/qhuang/workspace/dpdk-2.2.0/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
+insmod /home/qhuang/workspace/dpdk-2.2.0/lib/librte_vhost/eventfd_link/eventfd_link.ko
+
+/home/qhuang/workspace/dpdk-2.2.0/tools/dpdk_nic_bind.py --bind=vfio-pci eth0
+/home/qhuang/workspace/dpdk-2.2.0/tools/dpdk_nic_bind.py --status
+
+#### setup ovsdb
+rm -f /usr/local/etc/openvswitch/conf.db
+ovsdb-tool create /usr/local/etc/openvswitch/conf.db /usr/local/share/openvswitch/vswitch.ovsschema
+
+### open ovsdb server (no SSL) 
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+    --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+    --pidfile --detach --log-file
+
+### open ovsctl
+ovs-vsctl --no-wait init
+
+### open switched
+/usr/local/sbin/ovs-vswitchd --dpdk  -c 0x1 -n 4 --socket-mem 1024,0 -- unix:/usr/local/var/run/openvswitch/db.sock --pidfile --detach
+#/usr/local/sbin/ovs-vswitchd --dpdk  -c 0x1 -n 4 -- unix:/usr/local/var/run/openvswitch/db.sock --pidfile --detach
+
+ovs-vsctl add-br br0 -- set bridge br0 datapath_type=netdev
+ovs-vsctl add-port br0  vhost-user1 -- set interface vhost-user1 type=dpdkvhostuser
+ovs-vsctl add-port br0  vhost-user2 -- set interface vhost-user2 type=dpdkvhostuser
+#ovs-vsctl add-port br0  vhost-user2 -- set interface vhost-user2 type=dpdkvhostuser
+#ovs-vsctl add-port br0  dpdk0 -- set interface dpdk0 type=dpdk
---
+++ stop_ovs_dpdk.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ovs-vsctl del-br br0
+ovs-appctl -t ovs-vswitchd exit
+ovs-appctl -t ovsdb-server exit
